7d47f52f11af3d9fbfaf044401af3f3fc11ace14
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-02-16 21:25:05 +0000

    Fixing issues with search implementation in api/v1/views/places.py

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -6,9 +6,12 @@ from flask import request
 
 from api.v1.views import app_views
 from models import storage
+from models import storage_t
 from models.place import Place
 from models.city import City
 from models.user import User
+from models.amenity import Amenity
+from models.state import State
 
 
 @app_views.route(
@@ -100,3 +103,73 @@ def put_place(place_id):
             setattr(place, key, value)
     storage.save()
     return jsonify(place.to_dict()), 200
+
+
+@app_views.route("/places_search", methods=["POST"], strict_slashes=False)
+def places_search():
+    """
+    Handle POST requests to "/places_search" to retrieve all Place objects
+    depending on the JSON in the body of the request
+    """
+    data = request.get_json()
+    if data is None:
+        abort(400, description="Not a JSON")
+
+    if data == {} or all(
+            isinstance(value, list) and not value
+            for value in data.values()):
+        places = list(storage.all(Place).values())
+        return jsonify([place.to_dict() for place in places])
+
+    s_cities = []
+    if data.get("states"):
+        states = [storage.get(State, state_id)
+                  for state_id in data["states"]
+                  if storage.get(State, state_id)]
+        s_cities = [city for state in states for city in state.cities]
+        #places = [place for city in s_cities for place in city.places]
+
+    cities = []
+    if data.get("cities"):
+        cities = [storage.get(City, city_id)
+                  for city_id in data["cities"]
+                  if storage.get(City, city_id)]
+        #places = [place for city in cities for place in city.places]
+
+    all_cities = set(s_cities + cities)
+    places = [place for city in all_cities for place in city.places]
+
+    if data.get("amenities"):
+        #amenities = [storage.get(Amenity, amenity_id)
+        #             for amenity_id in data["amenities"]
+        #             if storage.get(Amenity, amenity_id)]
+
+        amenities = []
+        for amenity_id in data["amenities"]:
+            amenity = storage.get(Amenity, amenity_id)
+            amenity = amenity.to_dict()
+            amenity_ids = amenity["id"]
+            amenities.append(amenity_ids)
+
+        if not places:
+            places = list(storage.all(Place).values())
+
+        #filtered_places = []
+        #p_amen = []
+        #for place in places:
+        #    if storage_t == "db":
+        #        place_amenities = place.amenities
+        #    else:
+        #        place_amenities = [storage.get(Amenity, amenity_id)
+        #                           for amenity_id in place.amenities]
+        #    place_amenities = place_amenities.to_dict()
+        #    p_amen.append(place_amenities)
+
+            #place_amenities = place_amenities
+         #   if all(amenity in p_amen for amenity in amenities):
+          #      filtered_places.append(place)
+
+        #places = filtered_places
+        return places
+
+    return jsonify([place.to_dict() for place in places])
