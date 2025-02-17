b4cd549d823fb2b2c50fb12d690e93e8f3731cc5
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-02-17 05:02:47 +0000

    Add test cases to tests/test_models/test_engine/test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -78,11 +78,37 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_all_no_class(self):
         """Test that all returns all rows when no class is passed"""
+        pass
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_new(self):
         """test that new adds an object to the database"""
+        pass
 
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+        pass
+
+        @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_get(self):
+        """Test that retrieves object from file.json"""
+        state = State(name="Alberta")
+        models.storage.new(state)
+        models.storage.save()
+
+        state_object = models.storage.get(State, state.id)
+        self.assertEqual(state, state_object)
+
+    @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+    def test_count(self):
+        """Test that counts objects in file.json"""
+        all_objects = len(models.storage.all())
+        count_objects = models.storage.count()
+
+        self.assertEqual(all_objects, count_objects)
+
+        all_states = len(models.storage.all(State))
+        count_states = models.storage.count(State)
+
+        self.assertEqual(all_states, count_states)
